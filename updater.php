77b2f7474216320b22282431aa827fc0b525a849
joshwayman <nowayman121@gmail.com>
2021-07-08 14:03:15 +1000

    Update updater.php

--- updater.php
+++ updater.php
@@ -113,7 +113,7 @@ class BEECH_Updater {
                     'name'				=> $this->plugin["Name"],
                     'slug'				=> $this->basename,
                     'requires'					=> '5.1',
-                    'tested'						=> '5.7.0',
+                    'tested'						=> '5.7.2',
                     'rating'						=> '100.0',
                     'num_ratings'				=> '5',
                     'downloaded'				=> '5',
