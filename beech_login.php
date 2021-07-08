1d6ce4283b999cb452b66bf69da34a56af0f128e
joshwayman <nowayman121@gmail.com>
2021-07-08 14:02:48 +1000

    Update beech_login.php

--- beech_login.php
+++ beech_login.php
@@ -3,7 +3,7 @@
  * Plugin Name: BEECH Login
  * Plugin URI: https://beech.agency
  * Description: Makes your login screen look amazing!
- * Version: 1.5
+ * Version: 2.0
  * Author: BEECH
  * Author URI: https://beech.agency
  */
